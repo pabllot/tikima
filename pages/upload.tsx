69405c51eb4cdf5657e669a1111c3757d8b3890e
Pabllot07 <pabllot07@outlook.com>
2022-07-29 19:03:32 -0300

    part one complete

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -14,6 +14,12 @@ const Upload = () => {
     const [isLoading, setIsLoading] = useState(false)
     const [videoAsset, setVideoAsset ] = useState<SanityAssetDocument | undefined>()
     const [wrongFileType, setWrongFileType] = useState(false)
+    const [caption, setCaption] = useState('')
+    const [category, setCategory] = useState(topics[0].name)
+    const [savingPost, setSavingPost] = useState(false)
+
+    const { userProfile }: { userProfile: any} = useAuthStore()
+    const router = useRouter()
 
     const uploadVideo = async (e: any) => {
         const selectedFile = e.target.files[0]
@@ -31,6 +37,34 @@ const Upload = () => {
         } else {
             setIsLoading(false)
             setWrongFileType(true)
+        }
+    }
+
+    const handlePost = async () => {
+        if(caption && videoAsset?._id && category) {
+            setSavingPost(true)
+
+            const document = {
+                _type: 'post',
+                caption,
+                video: {
+                    _type: 'file',
+                    asset: {
+                        _type: 'reference',
+                        _ref: videoAsset?._id
+                    }
+                },
+                userId: userProfile?._id,
+                postedBy: {
+                    _type: 'postedBy',
+                    _ref: userProfile?._id
+                },
+                topic: category
+            }
+
+            await axios.post('http://localhost:3000/api/post', document)
+
+            router.push('/')
         }
     }
 
@@ -95,13 +129,13 @@ const Upload = () => {
                     <label className='text-md font-medium'>Caption</label>
                     <input
                     type='text'
-                    value=''
-                    onChange={() => {}}
+                    value={caption}
+                    onChange={(e) => setCaption(e.target.value)}
                     className='rounded outline-none text-md border-2 border-gray-200 p-2'
                     />
                     <label>Choose a Category</label>
                     <select 
-                      onChange={() => {}}
+                      onChange={(e) => setCategory(e.target.value)}
                       className='outline-none border-2 border-gray-200 text-md capitalize lg:p-4 p-2 ounded cursor-pointer'
                     >
                         {topics.map((topic) => (
@@ -123,7 +157,7 @@ const Upload = () => {
                             Discard
                         </button>
                         <button
-                          onClick={() => {}}
+                          onClick={handlePost}
                           type='button'
                           className='bg-[#F51997] text-white text-md font-medium p-2 rounded w-28 lg:w-44 outline-none'
                         >
