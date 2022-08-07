744895df989580d132ac3c3cda095734bbb1fa60
Pabllot07 <pabllot07@outlook.com>
2022-08-07 16:42:44 -0300

    final

--- components/SuggestedAccounts.tsx
+++ components/SuggestedAccounts.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 import Image from 'next/image'
-import Link from 'next/Link'
+import Link from 'next/link'
 import { GoVerified } from 'react-icons/go'
 
 import useAuthStore from '../store/authStore'
